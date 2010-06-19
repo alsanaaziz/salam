4a8692c5721a6218b2eacba8fd5e4045541c3b9e
Aziz <alsana.aziz@gmail.com>
2010-06-19 19:12:45 +0200

    scripts: books.py: added return statement if too few args.

--- scripts/books.py
+++ scripts/books.py
@@ -10,17 +10,18 @@ def main():
   help = 'books.py {hassan,mowtq}'
   if len(argv) <= 1:
     print help
+    return
 
   arg1 = argv[1]
   if arg1 == 'hassan':
     fname = 'HassansBook-en.tex'
-    text = open('books/'+fname).read()
-    text = replace_QRef(text.decode("utf-8"))
+    text = open('books/'+fname).read().decode("utf-8")
+    text = replace_QRef(text)
     open(fname, "w").write(text.encode("utf-8"))
   elif arg1 == 'mowtq':
     fname = 'My_Ordeal_With_The_Quran.tex'
-    text = open('books/'+fname).read()
-    text = replace_QRef(text.decode("utf-8"))
+    text = open('books/'+fname).read().decode("utf-8")
+    text = replace_QRef(text)
     open(fname, "w").write(text.encode("utf-8"))
 
 if __name__ == '__main__':
