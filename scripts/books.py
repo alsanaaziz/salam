4d00af1551367578fc9ac7de61cd2e7da4024baa
Aziz <alsana.aziz@gmail.com>
2010-06-19 19:11:49 +0200

    scripts: added book.py module.

---
+++ scripts/books.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# Author: AlsanaAziz
+from __future__ import unicode_literals
+import re
+from latex import *
+
+def main():
+  from sys import argv
+  help = 'books.py {hassan,mowtq}'
+  if len(argv) <= 1:
+    print help
+
+  arg1 = argv[1]
+  if arg1 == 'hassan':
+    fname = 'HassansBook-en.tex'
+    text = open('books/'+fname).read()
+    text = replace_QRef(text.decode("utf-8"))
+    open(fname, "w").write(text.encode("utf-8"))
+  elif arg1 == 'mowtq':
+    fname = 'My_Ordeal_With_The_Quran.tex'
+    text = open('books/'+fname).read()
+    text = replace_QRef(text.decode("utf-8"))
+    open(fname, "w").write(text.encode("utf-8"))
+
+if __name__ == '__main__':
+  main()
